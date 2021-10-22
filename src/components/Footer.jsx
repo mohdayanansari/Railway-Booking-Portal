aa268f45a8c528c5bd02faf8299f5efd438dd311
Ayan Ansari <1100zaid@gmail.com>
2021-10-22 23:18:23 +0530

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="fixed bottom-0 flex items-center justify-center w-full py-2 text-xl font-bold text-white">
+    <div className="fixed bottom-0 flex items-center justify-center w-full py-2 text-xl font-bold text-black my_footer">
       ©️Railway Booking Portal | {new Date().getFullYear()}
     </div>
   );
