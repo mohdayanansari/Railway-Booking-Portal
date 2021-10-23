33867d1c8ebdc9ee666e498fa994d67a0a8238d3
Shekhar Tyagi <notshekahr@gmail.com>
2021-10-23 12:11:57 +0530

    new

---
+++ src/apis/index.js
@@ -0,0 +1,3 @@
+
+export const booking_api = "http://127.0.0.1:3001/book"
+export const _availableSeats = "http://127.0.0.1:3001/seats"
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,9 @@
-const Button = ({ btnName }) => {
+const Button = ({ btnName, type, ...params }) => {
   return (
     <>
       <button
-        type="submit"
+        type={!type ? "" : type}
+        {...params}
         className="w-40 py-4 text-white transform shadow from-blue-500 to-blue-800 bg-gradient-to-b hover:scale-98 focus:scale-95 rounded-xl focus:ring focus:ring-offset-2 focus:ring-blue-500"
       >
         {btnName}
