029e60d95e8f64fa054d3f2a75204ee7c6f1524a
Shekhar Tyagi <notshekahr@gmail.com>
2021-10-23 12:27:22 +0530

    new

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,3 +1,3 @@
 
-export const booking_api = "http://127.0.0.1:3001/book"
-export const _availableSeats = "http://127.0.0.1:3001/seats"
+export const booking_api = "https://typical-denim-krypton.glitch.me/book"
+export const _availableSeats = "https://typical-denim-krypton.glitch.me/seats"
